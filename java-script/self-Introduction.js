dc831be138637094037738a67668b51a8615190c
てぃ <t.u.gundam2008@gmail.com>
2025-08-18 09:25:37 +0900

    Update self-Introduction.js

--- java-script/self-Introduction.js
+++ java-script/self-Introduction.js
@@ -1,1 +1,13 @@
+const text = "名前: てぃ\n年齢: 17歳\n好きなもの: 魔法";
+const target = document.getElementById("typewriter");
+let i = 0;
 
+function typeWriter() {
+  if (i < text.length) {
+    target.textContent += text.charAt(i);
+    i++;
+    setTimeout(typeWriter, 100); // 表示速度（ms）
+  }
+}
+
+typeWriter();
