96167890cdba2b976fd367ef9dcab7cadb1c4552
てぃ <t.u.gundam2008@gmail.com>
2025-08-18 09:55:19 +0900

    Update self-Introduction.js

--- java-script/self-Introduction.js
+++ java-script/self-Introduction.js
@@ -1,13 +1,19 @@
-const text = "名前: てぃ<br>年齢: 17歳<br>好きなもの: 魔法";
-const target = document.getElementById("intro-typewriter");
-let i = 0;
-
 function intro_typeWriter() {
-  if (i < text.length) {
-    target.textContent += text.charAt(i);
-    i++;
-    setTimeout(intro_typeWriter, 100); // 表示速度（ms）
+  const text = "名前: てぃ<br>年齢: 17歳<br>好きなもの: 魔法";
+  const target = document.getElementById("intro-typewriter");
+  let i = 0;
+  let htmlText = "";
+
+  function typing() {
+    if (i < text.length) {
+      htmlText += text.charAt(i);
+      target.innerHTML = htmlText;
+      i++;
+      setTimeout(typing, 100);
+    }
   }
+
+  typing();
 }
 
-intro_typeWriter();
+document.addEventListener("DOMContentLoaded", intro_typeWriter);
