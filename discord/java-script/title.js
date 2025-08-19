ddab21ff7322f13d346f2fd33c9c2a4c9bde3e6d
てぃ <t.u.gundam2008@gmail.com>
2025-08-19 11:25:48 +0900

    Create title.js

---
+++ discord/java-script/title.js
@@ -0,0 +1,17 @@
+function title_typeWriter() {
+  const titletext = "てぃのDiscord";
+  const target = document.getElementById("title_typewriter");
+  let i = 0;
+
+  function typing() {
+    if (i < titletext.length) {
+      target.textContent += titletext.charAt(i);
+      i++;
+      setTimeout(typing, 100);
+    }
+  }
+
+  typing();
+}
+
+document.addEventListener("DOMContentLoaded", title_typeWriter);
