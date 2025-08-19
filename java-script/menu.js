30c65ef61f29ead171480e89615f09934a77f43e
てぃ <t.u.gundam2008@gmail.com>
2025-08-19 13:40:01 +0900

    Update menu.js

--- java-script/menu.js
+++ java-script/menu.js
@@ -4,3 +4,9 @@ const sideMenu = document.getElementById("sideMenu");
 menuButton.addEventListener("click", () => {
   sideMenu.classList.toggle("open");
 });
+
+document.addEventListener("click", (e) => {
+  if (sideMenu.classList.contains("open") && !sideMenu.contains(e.target) && e.target !== menuButton) {
+    sideMenu.classList.remove("open");
+  }
+});
