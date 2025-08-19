c59ea717e0c03ffe793f9d8220a5ad40f8d193b9
てぃ <t.u.gundam2008@gmail.com>
2025-08-19 13:18:27 +0900

    Create cosmos.js

---
+++ discord/java-script/cosmos.js
@@ -0,0 +1,47 @@
+function COSMOS_typeWriter() {
+  const texts = [
+    "サーバー名: COSMOS SERVER",
+    "設立: 2023/08/17",
+    "概要:"
+  ];
+  const target = document.getElementById("cosmos_typewriter");
+  let textIndex = 0;
+  let charIndex = 0;
+  let htmlText = "";
+
+  function typing() {
+    if (textIndex < texts.length) {
+      const currentText = texts[textIndex];
+      if (charIndex < currentText.length) {
+        htmlText += currentText.charAt(charIndex);
+        target.textContent = htmlText;
+        charIndex++;
+        setTimeout(typing, 100);
+      } else {
+        htmlText += "\n";
+        textIndex++;
+        charIndex = 0;
+        setTimeout(typing, 500);
+      }
+    }
+  }
+
+  typing();
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const target = document.getElementById("cosmos_typewriter");
+
+  const observer = new IntersectionObserver((entries, obs) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        COSMOS_typeWriter();
+        obs.unobserve(entry.target); // 一度だけ発動
+      }
+    });
+  }, {
+    threshold: 0.6 // 要素が60%以上見えたら発動
+  });
+
+  observer.observe(target);
+});
