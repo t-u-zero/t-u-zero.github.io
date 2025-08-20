9ab6ed7f5455cf53f454b6d5972e88450d5f2ad1
てぃ <t.u.gundam2008@gmail.com>
2025-08-20 18:08:24 +0900

    Create title.js

---
+++ discord/cosmos/java-script/title.js
@@ -0,0 +1,22 @@
+function COSMOS_title_typeWriter() {
+  const titletext = "COSMOS SERVER";
+  const lasttext = "𝑪𝑶𝑺𝑴𝑶𝑺 𝑺𝑬𝑹𝑽𝑬𝑹";
+  const target = document.getElementById("cosmos_title_typewriter");
+  let i = 0;
+
+  function typing() {
+    if (i < titletext.length) {
+      target.textContent += titletext.charAt(i);
+      i++;
+      setTimeout(typing, 100);
+    } else {
+      // 打ち終わったら置き換える
+      setTimeout(() => {
+        target.textContent = lasttext;
+      }, 250);
+    }
+  }
+  typing();
+}
+
+document.addEventListener("DOMContentLoaded", COSMOS_title_typeWriter);
