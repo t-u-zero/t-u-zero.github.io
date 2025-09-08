9ed91689d8ff6467f41b26b5c892a8367e2a8fa1
てぃ <t.u.gundam2008@gmail.com>
2025-09-08 11:21:05 +0900

    Update title.js

--- discord/voynich/java-script/title.js
+++ discord/voynich/java-script/title.js
@@ -1,1 +1,22 @@
+function Voynich_title_typeWriter() {
+  const titletext = "Voynich";
+  const lasttext = "🌏Voynich💻";
+  const target = document.getElementById("voynich_title_typewriter");
+  let i = 0;
 
+  function typing() {
+    if (i < titletext.length) {
+      target.textContent += titletext.charAt(i);
+      i++;
+      setTimeout(typing, 100);
+    } else {
+      // 打ち終わったら置き換える
+      setTimeout(() => {
+        target.textContent = lasttext;
+      }, 250);
+    }
+  }
+  typing();
+}
+
+document.addEventListener("DOMContentLoaded", Voynich_title_typeWriter);
